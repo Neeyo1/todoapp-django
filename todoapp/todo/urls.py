f4e1dd043413d72ef34dc4c4e2e676c0c6afdcf4
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-30 14:27:39 +0200

    Login and register

--- todoapp/todo/urls.py
+++ todoapp/todo/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     #main page
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    #login
+    path("login/", views.login_to_page, name="login_to_page"),
+    #logout
+    path("logout/", views.logout_from_page, name="logout_from_page"),
+    #register
+    path("register/", views.register_to_page, name="register_to_page"),
+
 ]
--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Todo
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -9,3 +12,41 @@ def index(request):
     todo_items = Todo.objects.all()
     context['todo_items'] = todo_items
     return render(request, "todo/index.html", context)
+
+def login_to_page(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("Success")
+            next_url = request.GET.get('next') or "/"
+            return HttpResponseRedirect(next_url)
+        else:
+            error_message = "Incorrect login or password"
+            context["error_message"] = error_message
+            return render(request, "todo/login_form.html", context)
+    return render(request, "todo/login_form.html", context)
+
+def logout_from_page(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
+def register_to_page(request):
+    context = {}
+    form = UserCreationForm()
+    context["form"] = form
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            error_message = "Error during registration"
+            context["error_message"] = error_message
+            context["form"] = form
+            return render(request, "todo/register_form.html", context)
+    return render(request, "todo/register_form.html", context)
