9f83c63a189eed22178fe88ac5fc8367fefc1777
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-30 14:10:52 +0200

    Display todo items on main page

--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
     context = {}
+    todo_items = Todo.objects.all()
+    context['todo_items'] = todo_items
     return render(request, "todo/index.html", context)
