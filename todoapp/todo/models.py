0fbd57773b735b652ffeedc51f5fe712a7f390d7
Neeyo1 <jakub.nycz99@gmail.com>
2023-07-30 13:51:06 +0200

    Todo note model

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Todo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+    def is_completed(self):
+        return self.completed
